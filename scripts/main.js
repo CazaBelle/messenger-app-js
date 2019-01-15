49b0405f5958fdbec0cb73a388d90fc6f884ccc0
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-01-15 15:25:52 +0000

    partial implementation of grabbing user text and initiating a function

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,28 @@
-document.body.innerText="Hello Other World"
+button = document.getElementById('button');
+
+button.addEventListener('click', function() { 
+	var class_test = new Message("Yo bishbish", new Date);
+   	console.log(class_test) });
+
+class Message {
+	constructor(content, date){
+		this.content = content;
+		this.date = date;
+	}
+}
+
+// function grabInput()
+// {
+// var written_message = document.getElementById("user_input");
+// console.log(written_message);
+// }
+
+
+function grabInput()
+{
+var written_message = document.getElementById('user_input').value;
+console.log(written_message);
+}
+
+// var text = document.getElementById('user_input').value;
+// return text
