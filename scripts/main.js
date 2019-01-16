a2998a8859693b507131f9d63fec3475f4fc213a
Carol Gardiner <carolan.gardiner@gmail.com>
2019-01-16 20:53:17 +0000

    numbers appearing in reverse chronilogical order

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 arr = [];
 const submit_button = document.getElementById('button'); 
-const current = document.getElementById('list');
+const currentLi = document.getElementById('list');
 
 class Message {
 	constructor(content, date){
@@ -9,20 +9,21 @@ class Message {
 	}
 }
 
-
 const handleClick = () => {
 	const grabinput = document.getElementById('user_input').value;
 	const create_message = new Message(grabinput, new Date );
 	arr.push(create_message)
 	displayMessage(create_message);
-		
 	}; 
+
+	
 
 	const displayMessage = (message) => {
 		const newLi = document.createElement('li');
-		const newContent = document.createTextNode(`${message.content} at ${message.date.toDateString()}`);
+		const newContent = document.createTextNode(`${message.content} at ${message.date}`);
+		// toDateString() this formats date
 		newLi.appendChild(newContent);
-		current.appendChild(newLi)
+		currentLi.appendChild(newLi)
 	};
 
 	submit_button.addEventListener('click', handleClick);
