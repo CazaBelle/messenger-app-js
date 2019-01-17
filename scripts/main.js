b91f6515dd4f0c1e4b48f65eee08194d59980e67
Carol Gardiner <carolan.gardiner@gmail.com>
2019-01-17 14:48:32 +0000

    added auto date to message

--- scripts/main.js
+++ scripts/main.js
@@ -5,18 +5,16 @@ const currentLi = document.getElementById('list');
 class Message {
 	constructor(content, date){
 		this.content = content;
-		this.date = date;
+		this.date = Date();
 	}
 }
 
 const handleClick = () => {
 	const grabinput = document.getElementById('user_input').value;
-	const create_message = new Message(grabinput, new Date );
+	const create_message = new Message(grabinput);
 	arr.push(create_message)
 	displayMessage(create_message);
 	}; 
-
-	
 
 	const displayMessage = (message) => {
 		const newLi = document.createElement('li');
